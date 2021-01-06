8c886bf996df905b2fb059405e72feb4ffb8dc7a
meltem14 <m.ataman14@outlook.com>
2021-01-06 21:57:05 +0300

    her siparişi yönetim panelinde ayrı div olarak ekleme

--- views/YonPanel/sayfalar/siparis.php
+++ views/YonPanel/sayfalar/siparis.php
@@ -10,105 +10,137 @@
 
    <?php 
 
-   // aynı sipariş numarısını ayrı ayrı göstermek yerine 1 sipariş olarak gösterme
+   // aynı sipariş numarısından birden fazla varsa bunu tek  1 sipariş olarak gösterme
    $dizim = array(); // sip_no tutar
    $sayi = 0;
    foreach ($veri["data"] as $value):
 
-      if (!in_array($value["siparis_no"], $dizim)):
+      if (!in_array($value["siparis_no"], $dizim)): // sipariş numarası dizide daha önce yoksa
          $sayi++;
       endif;
-   $dizim[] = $value["siparis_no"];
+
+   $dizim[] = $value["siparis_no"]; // döngüden gelen sip noları diziye tanımladım
    endforeach;
 
    ?>
 
 
-         <!-- BAŞLIK -->
-         <div class="row text-left border-bottom-mvc mb-2">
+      <!-- BAŞLIK -->
+      
+      <div class="row text-left border-bottom-mvc mb-2">
 
-            <div class="col-xl-4 col-md-12 mb-12 border-left-mvc text-left p-2 mb-2">
+         <div class="col-xl-4 col-md-12 mb-12 border-left-mvc text-left p-2 mb-2">
 
-               <h1 class="h3 mb-0 text-gray-800">
+            <h1 class="h3 mb-0 text-gray-800">
 
-                  <i class="fas fa-th basliktext"></i> SİPARİŞLER
-               </h1>
+               <i class="fas fa-th basliktext"></i> SİPARİŞLER
 
-            </div>
+            </h1>
 
-            <div class="col-xl-4 col-md-12 mb-12 p-2">
+         </div>
 
-               <h1 class="h3 mb-0 text-gray-800">Toplam sipariş : 
+         <div class="col-xl-4 col-md-12 mb-12 p-2">
 
-               <?php 
+            <h1 class="h3 mb-0 text-gray-800">Toplam sipariş : 
 
-                  echo $sayi;
+            <?php 
 
-               ?>
-               
-               </h1>
+               echo $sayi;
 
-            </div>
+            ?>
+            
+            </h1>
+
+         </div>
 
 
-            <div class="col-xl-4 col-md-12 mb-12 text-right">
+         <div class="col-xl-4 col-md-12 mb-12 text-right">
 
-               <div class="row">
+            <div class="row">
 
-                  <div class="col-xl-8">
+               <div class="col-xl-8">
 
-                     <form action="#" method="post">
+                  <form action="#" method="post">
 
-                     <input type="text" class="form-control" name="sipno" placeholder="Sipariş numarası" />
-
-                  </div>
-
-                  <div class="col-xl-4">
-
-                     <input type="submit" value="ARA" class="btn btn-sm btn-mvc btn-block mt-1" />
-
-                     </form>
-
-                  </div>
+                  <input type="text" class="form-control" name="sipno" placeholder="Sipariş numarası" />
 
                </div>
-            </div>
 
+               <div class="col-xl-4">
+
+                  <input type="submit" value="ARA" class="btn btn-sm btn-mvc btn-block mt-1" />
+
+                  </form>
+
+               </div>
+
+            </div>
          </div>
 
       </div>
       <!-- BAŞLIK -->
+
+      <?php
+
+      $dizi2 = array();
+
+      foreach ($veri["data"] as $value):
+         // sipariş numarası dizi2de yoksa yeni ürünü alt dive atacak
+         if (!in_array($value["siparis_no"], $dizi2)): 
+           
+
+      ?>
+
 
       <!-- SİPARİŞİN İSKELETİ BAŞLIYOR -->
       <div class="row arkaplan p-1 mt-2 pb-0">
 
          <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
 
-            <span>Sipariş No :</span> <span class="spantext">454154</span>
+            <span>Sipariş No :</span> <span class="spantext"><?php echo $value["siparis_no"]; ?></span>
+
+         </div>
+
+         <div class="col-xl-1 col-lg-1 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
+
+            <span>Üye id :</span> <span class="spantext">21554</span>
+
+         </div>
+
+         <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
+
+            <span>Kargo Durumu :</span> <span class="spantext">
+            <?php 
+            
+            switch($value["kargodurum"]) :
+              
+            case "0":
+            echo "Tedarik Sürecinde";
+            break;
+
+            case "1":
+            echo "Paketleniyor";
+            break;
+
+            case "2":
+            echo "Kargoya verildi";
+            break;
+            endswitch;
+
+            ?>
+            </span>
 
          </div>
 
          <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
 
-            <span>Üye id :</span> <span class="spantext">454154</span>
+            <span>Ödeme Türü :</span> <span class="spantext"><?php echo $value["odemeturu"]; ?></span>
 
          </div>
 
          <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
 
-            <span>Kargo Durumu :</span> <span class="spantext">454154</span>
-
-         </div>
-
-         <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
-
-            <span>Ödeme Türü :</span> <span class="spantext">454154</span>
-
-         </div>
-
-         <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 pt-3 geneltext bg-gradient-mvc">
-
-            <span>Tarih :</span> <span class="spantext">454154</span>
+            <span>Tarih :</span> <span class="spantext"><?php echo $value["tarih"]; ?></span>
 
          </div>
 
@@ -130,23 +162,23 @@
 
             </div>
 
-            <div class="row border border-light">   
+            <?php
 
-               <div class="col-lg-3 text-dark kalinyap p-2">gömlek</div>
-               <div class="col-lg-3 text-dark kalinyap p-2">2</div>
-               <div class="col-lg-3 text-dark kalinyap p-2">3</div>
-               <div class="col-lg-3 text-dark kalinyap p-2">10</div>
+               endif;
+               echo '<div class="row border border-light">   
 
-            </div>
+               <div class="col-lg-3 text-dark kalinyap p-2">'.$value["urunad"].'</div>
+               <div class="col-lg-3 text-dark kalinyap p-2">'.$value["urunadet"].'</div>
+               <div class="col-lg-3 text-dark kalinyap p-2">'.$value["urunfiyat"].'</div>
+               <div class="col-lg-3 text-dark kalinyap p-2">'.$value["toplamfiyat"].'</div>
 
-            <div class="row border border-light">   
+               </div>';
+               // yazma işlemi bittikten sonra aynı sipariş noyu diziye ataarak döngüye devam eder
+               $dizi2[] = $value["siparis_no"];
+               endforeach;
 
-               <div class="col-lg-3 text-dark kalinyap p-2">hırka</div>
-               <div class="col-lg-3 text-dark kalinyap p-2">3</div>
-               <div class="col-lg-3 text-dark kalinyap p-2">5</div>
-               <div class="col-lg-3 text-dark kalinyap p-2">15</div>
 
-            </div>
+            ?>
 
             <!-- TOPLAM FİYAT -->      
             <div class="row"> 
@@ -164,7 +196,7 @@
       </div>
       <!-- SİPARİŞİN İSKELETİ BİTİYOR -->
 
-   </div>
+   
 
 </div>
 <!-- /.row bitiyor -->
