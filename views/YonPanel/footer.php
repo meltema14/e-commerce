1a5da7c5b6d808579f0ff27da6e8a4c80e354d42
meltem14 <m.ataman14@outlook.com>
2021-01-06 14:08:19 +0300

    yönetim paneli footer ve header dosyaları

---
+++ views/YonPanel/footer.php
@@ -0,0 +1,36 @@
+<!--   YÖNETİM PANELİ FOOTER   -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Meltem Ataman<br /><br />
+            MVC YÖNETİM PANELLİ E-TİCARET PROJESİ 2021</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?php  echo URL; ?>/views/YonPanel/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php  echo URL; ?>/views/YonPanel/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?php  echo URL; ?>/views/YonPanel/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?php  echo URL; ?>/views/YonPanel/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
